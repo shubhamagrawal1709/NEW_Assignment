89135af845b58d96960e48e51a5b4baeb0cab237
Shubham Agrawal <shubham.agrawal@airtel.com>
2020-09-18 23:49:26 +0530

    commit

--- Test_Example/target/cucumberHtmlReport/report.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("features/my_second.feature");
-formatter.feature({
-  "line": 1,
-  "name": "N",
-  "description": "      to check weather comparision",
-  "id": "n",
-  "keyword": "Feature"
-});
-});
